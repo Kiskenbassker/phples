418a46b6e5065e8c288fc138bba03c2a0bec908b
Kiskenbassker <kiskenbassker@gmail.com>
2017-07-03 01:40:45 +0300

    Changing cookies

--- cookie.php
+++ cookie.php
@@ -1,9 +1,9 @@
 <?php
 if((!isset($_COOKIE['party']))&&(isset($_POST['send']))) {
     $name = $_POST['name'];
-    $partying[] = $name;
-    setcookie('party');
+    setcookie('party', $name);
 }
+date_default_timezone_set('Europe/Kiev');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,10 +18,15 @@ if((!isset($_COOKIE['party']))&&(isset($_POST['send']))) {
     <input type="text" name="name" id="name" />
     <input type="submit" value="Send" name="send" />
     </form>
-    <?php
-    foreach($partying as $party) {
-        echo $party . "<br/>";
-    }
-    ?>
+    <?php 
+        $current_date = date('H:i');
+          $start_date = '01:00';
+          $end_date = '02:00';
+        
+        if(isset($_COOKIE['party']) && ($current_date >= $start_date) && ($current_date <= $end_date))
+            echo "<br/>You're part of our party!<br/>";
+        else
+            echo "<br/>You're not partying with us!<br/>";
+    ?> 
 </body>
 </html>
