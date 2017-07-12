c8b843a1ad591fe80891deb778b5319b795451f9
Kiskenbassker <kiskenbassker@gmail.com>
2017-07-12 21:05:18 +0300

    Adding session script

--- session.php
+++ session.php
@@ -1,37 +1,43 @@
 <?php
-if((isset($_POST['send'])) && (!in_array($_POST['name'], $_COOKIE))) {
-    $random = rand();
-    setcookie("party{$random}", $_POST['name']);
+session_start();
+
+if(isset($_POST['send']) && (!in_array($_POST['name'], $_SESSION['party']))) {
+    if(!isset($_SESSION['party']))
+        $_SESSION['party'] = array();
+    
+    array_push($_SESSION['party'], $_POST['name']);
 }
+
 date_default_timezone_set('Europe/Kiev');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cookies Party</title>
+    <title>Sessions Party</title>
 </head>
 <body>
+  <?php var_dump($_SESSION['party']);
+    ?>
+  
    <p>Hello, welcome to our Sessions Party with Cookies and Lemonade!</p>
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-    <label for="name">Your name</label>
+    <label for="name">Your name, please!</label>
     <input type="text" name="name" id="name" />
     <input type="submit" value="Send" name="send" />
     </form>
     <?php 
         $current_date = date('H:i');
-          $start_date = '00:00';
-          $end_date = '02:10';
+          $start_date = '20:00';
+          $end_date = '22:00';
         
-        if(in_array($_POST['name'], $_COOKIE)) {
+        if(in_array($_POST['name'], $_SESSION['party'])) {
             if(($current_date >= $start_date) && ($current_date <= $end_date)) {
                 echo "<br/>You're part of our party!<br/>";
-                foreach($_COOKIE as $key => $value) {
-                    if(preg_match("/^party/", $key)) {
-                        echo $value . "<br/>";
-                    }
+                foreach($_SESSION['party'] as $key => $value) {
+                    echo $value . "<br/>";
                 } 
-                echo "are partying with you!";
+                echo "are partying today!";
             } else {
                 echo "<br/>The party time haven't come yet!";
             }
